71787db5a41eeba0cbde9f941634cbc4f5adbd5f
adimiupprix <miuprix@gmail.com>
2025-05-26 19:33:56 +0700

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-        <title>Garuda Ads - Solusi Bisnis Untuk Anda!</title>
+        <title>Garuda Ads - Solusi Bisnis Untuk Anda!!</title>
         <meta name="description" content="Temukan solusi terbaik dengan Garuda Ads. Dapatkan hasil maksimal dengan kelas online dan jasa kami." />
         <meta name="keywords" content="Garuda Ads, GarudadAdds, iklan online, jasa iklan online, periklanan digital,iklan google,iklan facebook,iklan instagram,periklanan sosial media,iklan mobile,iklan video,Iklan Internet,Periklanan Online,Iklan Digital,Iklan Google Ads,Iklan Facebook Ads,Iklan Instagram Ads,Iklan Mobile Ads,Iklan Video Ads,Jasa Iklan Online Murah,Periklanan Digital Indonesia,Iklan Internet yang Efektif,Periklanan Online untuk Bisnis,Iklan Digital untuk Meningkatkan Penjualan,Iklan Online Indonesia,Periklanan Digital Jakarta,Iklan Internet Bandung,Periklanan Online Surabaya,Iklan Digital Bali" />
 
